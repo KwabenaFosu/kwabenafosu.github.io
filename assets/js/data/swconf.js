ab166192db4ac8e02a25edf3155954159872a358
KwabenaFosu <KwabenaFosu@users.noreply.github.com>
2026-01-18 12:11:37 +0000

    deploy: c43c1863c8bbdd28bb1d69595054dacd02558e37

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1768738297',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
